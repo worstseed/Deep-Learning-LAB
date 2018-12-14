a6fa1401cd878774fb82c6acf7d0d0aafe3dcc01
worstseed <maciejanowski@gmail.com>
2018-12-14 20:16:46 +0100

    fix

--- exercise3/train_agent.py
+++ exercise3/train_agent.py
@@ -278,7 +278,7 @@ def train_model(X_train, y_train,
 
             log_dir = "./log/" + str(history_length) + "/"
             save_path = os.path.join(model_dir, "logg")
-            full_path = save_path '.txt'
+            full_path = save_path + '.txt'
             f = open(name, "a")
             f.write(msg.format(i + 1, training_accuracy[i], validation_accuracy[i]))
 
